17896cd42b95bf47f799a14179f37bf173750d5f
Technicallyaustin <Technicallyaustin@gmail.com>
2020-02-06 17:41:04 -0800

    Commit is Done.

---
+++ lib/xo_brewerlife.rb
@@ -0,0 +1,6 @@
+require "xo_brewerlife/version"
+
+module XoBrewerlife
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/xo_brewerlife/version.rb
@@ -0,0 +1,3 @@
+module XoBrewerlife
+  VERSION = "0.1.0"
+end
